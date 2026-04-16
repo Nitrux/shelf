b56a980d2530749beef0bffb2eeb106df43e9734
Uri Herrera <hello@uriherrera.com>
2026-04-15 18:01:25 -0600

    fix build deps

--- scripts/install-build-deps.sh
+++ scripts/install-build-deps.sh
@@ -43,9 +43,9 @@ $APT_COMMAND install -y - --no-install-recommends \
     libpoppler-dev \
     libpoppler-qt6-dev \
     lintian \
-    qml-module-qtgraphicaleffects \
-    qml-module-qtquick-controls2 \
-    qml-module-qtquick-shapes \
+    qml6-module-qt5compat-graphicaleffects \
+    qml6-module-qtquick-controls \
+    qml6-module-qtquick-shapes \
     qt6-base-dev \
     qt6-declarative-dev \
     qt6-multimedia-dev
